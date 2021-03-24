c8d6b40d87b5b712754095afc87400b467736690
Christine Antonio <christinejm.antonio@gmail.com>
2021-03-24 11:25:42 -0700

    test and implementation for POST /articles

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const { NODE_ENV } = require("./config");
 const ArticlesService = require("./articles-service");
 
 const app = express();
+const jsonParser = express.json();
 
 const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
@@ -35,6 +36,20 @@ app.get("/articles/:article_id", (req, res, next) => {
         });
       }
       res.json(article);
+    })
+    .catch(next);
+});
+
+app.post("/articles", jsonParser, (req, res, next) => {
+  // res.status(201).json({
+  // ...req.body,
+  // id: 12,
+  // });
+  const { title, content, style } = req.body;
+  const newArticle = { title, content, style };
+  ArticlesService.insertArticle(req.app.get("db"), newArticle)
+    .then((article) => {
+      res.status(201).location(`/articles/${article.id}`).json(article);
     })
     .catch(next);
 });
